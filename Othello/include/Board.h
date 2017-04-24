288ae54875d0be73e8f81d3758cc7dc5cc83d3f9
AntoineL1 <antoinelame1@gmail.com>
2017-04-24 02:34:04 +0200

    Création class et constantes nécessaires

---
+++ Othello/include/Board.h
@@ -0,0 +1,79 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <map>
+#include <vector>
+#include "constants.h"
+
+using namespace std;
+
+//Structure pour définir le x et le y joué pour obtenir ce board
+struct recordedMovePos
+{
+	int x;
+	int y;
+};
+
+/*
+	Classe qui modélise le plateau de jeu, et permet d'y effectuer
+	les actions élémentaires propres aux pions :
+
+	- Récupérer le contenu d'une cellule
+	- Récupérer les positions possibles d'un joueur
+	- Récupérer la liste des pions qui pourraient être mangés
+	- Manger un pion
+	- Ajouter un pion
+*/
+class Board
+{
+	public:
+		Board();
+		virtual ~Board();
+
+		// Ajouter un pion à une case donnée
+		bool move(int x, int y, int player);
+
+		// Retourne la liste des cellules où le joueur peut se déplacer
+		vector< map<char,int> > accessibles(int player);
+
+		// Retourne la liste des pions qui peuvent être mangés
+		vector< map<char,int> > eatables(int x_initial, int y_initial, int player);
+
+		// Mange un pion : il le transforme en la couleur opposé
+		bool eat(int x, int y);
+
+		// Retourne le contenu d'une cellule
+		int getCell(int x, int y);
+
+		// Setter pour le score associé
+		void setAssociatedScore(int _associatedScore);
+
+		// Getter pour le score associé
+		int getAssociatedScore();
+
+		// Setter pour l'état précédent
+		void setPreviousState(Board* _previousState);
+
+		// Getter pour l'état précédent
+		Board* getPreviousState();
+
+		// Setter pour le move effectué
+		void setRecordedMove(int _x, int _y);
+
+		// Getter pour le move effectué
+		recordedMovePos getRecordedMove();
+
+
+
+	private:
+		// Attributs
+		int cells[8][8];
+		//Score associé à l'état du board
+		int associatedScore;
+		//Renvoie à l'état précédent du board
+		Board* previousState;
+		//Initialization d'un objet de la structure
+		recordedMovePos recordedMove;
+};
+
+#endif // BOARD_H
---
+++ Othello/include/IA.h
@@ -0,0 +1,43 @@
+#ifndef IA_H
+#define IA_H
+#include <unordered_map>
+#include "Board.h"
+
+/*
+	Classe qui embarque les différentes intelligences artificielles.
+	Permet de jouer un tour automatiquement.
+
+	Pour cela, la classe utilise l'instance Board qui lui permet d'avoir
+	accès et de manipuler le plateau de jeu.
+*/
+class IA
+{
+	public:
+		IA();
+		IA(Board* p_board, int lvl);
+		virtual ~IA();
+
+		// Exécute la bonne IA selon le level
+		void run();
+
+	private:
+		// Pointeur vers l'instance du plateau de jeu. Permet de le manipuler
+		Board* board;
+
+		vector<Board*> tree;
+
+		// Attribut qui définit le niveau de l'IA
+		int level;
+
+		// Intelligence artificielle n°1, en privé et appellé par run()
+		void lvl_1();
+
+		// Intelligence artificielle n°2, en privé et appellé par run()
+		void lvl_2();
+
+		// Algorithme permettant de calculer le coup le plus efficace pour l'IA n°2
+		void minmax( multimap <int, Board>& _boardStates);
+
+};
+
+#endif // IA_H
